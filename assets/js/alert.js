407dd1ee1b8f9ea0f962aa910783aaa2d5bc2d5e
dorilex <108504269+dorilex@users.noreply.github.com>
2022-07-12 11:10:16 -0500

    subir wesite
    he subido todos mis archivos

---
+++ assets/js/alert.js
@@ -0,0 +1,12 @@
+swal("¡Bienvenido!", "......A la derecha encontraras un icono \"paint🎨\" para personalizar la vista de la pagina a tus gustos!")
+.then((value) => {
+    swal(`¡Gracias!`);
+  });
+  window.onunload=function(){swal('Vuelva en otro momento');}
+  //derechos reservados al copiar
+  document.body.oncopy = function() {
+swal('Todos los derechos reservados. Dorilexs ©️ 2021',  {
+    icon: "error",
+  });
+    return false;
+};
